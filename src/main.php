7f4dfd334144b40752b5609bbd8dea34ca0133da
Tomoya Koyanagi <tomk79@gmail.com>
2021-06-15 02:01:57 +0900

    script base

---
+++ src/main.php
@@ -0,0 +1,12 @@
+<?php
+$version = '0.0.1-alpha.1+dev';
+
+require_once(__DIR__.'/../vendor/autoload.php');
+
+echo '-------------------'."\n";
+echo 'rencon-builder v'.$version."\n";
+
+$fs = new \tomk79\filesystem();
+$req = new \tomk79\request();
+
+exit();

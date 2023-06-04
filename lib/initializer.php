4937e90f7fec077f0f1d44536bdeb2b5befeebce
Tomoya Koyanagi <tomk79@gmail.com>
2023-06-04 14:14:17 +0900

    add: initializer (3)

--- lib/initializer.php
+++ lib/initializer.php
@@ -70,27 +70,157 @@ Deny from all
 				'email' => $this->rencon->req()->get_param('admin_user_email'),
 				'role' => 'admin',
 			);
-			$result = $this->clover->auth()->create_admin_user( $user_info );
+			$result = $this->rencon->auth()->create_admin_user( $user_info ); // TODO: auth()->create_admin_user() は存在しない。実装する。
 			if( $result->result ){
 				header('Location:'.'?a=');
 				exit;
 			}
 		}
-echo "now develop // TODO: initialize page";
-return;
-		echo $this->clover->view()->bind(
-			'/system/initialize_admin_user.twig',
-			array(
-				'ADMIN_USER_NAME' => $this->rencon->req()->get_param('ADMIN_USER_NAME'),
-				'ADMIN_USER_ID' => $this->rencon->req()->get_param('ADMIN_USER_ID'),
-				'ADMIN_USER_LANG' => $this->rencon->req()->get_param('ADMIN_USER_LANG') ?? $this->rencon->lang(),
-				'admin_user_email' => $this->rencon->req()->get_param('admin_user_email'),
-				'message' => $result->message,
-				'errors' => $result->errors,
-				'url_backto' => '?',
-			)
-		);
+
+		header('Content-type: text/html');
+		ob_start();
+		?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title><?= htmlspecialchars( $this->app_info->name ?? '' ) ?></title>
+		<meta name="robots" content="nofollow, noindex, noarchive" />
+		<?= $this->mk_css() ?>
+	</head>
+	<body>
+		<div class="theme-container">
+			<h1><?= htmlspecialchars( $this->app_info->name ?? '' ) ?></h1>
+			<?php if( strlen($result->message ?? '') ){ ?>
+				<div class="alert alert-danger" role="alert">
+					<div><?= htmlspecialchars($result->message) ?></div>
+				</div>
+			<?php } ?>
+
+			<form action="?" method="post">
+<table>
+	<tr><th>Name:</th><td><input type="text" name="ADMIN_USER_NAME" value="<?= htmlspecialchars($this->rencon->req()->get_param('ADMIN_USER_NAME') ?? '') ?>" /></td>
+	<tr><th>ID:</th><td><input type="text" name="ADMIN_USER_ID" value="<?= htmlspecialchars($this->rencon->req()->get_param('ADMIN_USER_ID') ?? '') ?>" /></td>
+	<tr><th>Password:</th><td><input type="password" name="ADMIN_USER_PW" value="" /></td>
+</table>
+<p><button type="submit">Create User</button></p>
+<input type="hidden" name="a" value="<?= htmlspecialchars($this->rencon->req()->get_param('a') ?? '') ?>" />
+			</form>
+		</div>
+	</body>
+</html>
+<?php
+		$rtn = ob_get_clean();
+		print $rtn;
 		exit;
+	}
+
+	/**
+	 * CSSを生成
+	 */
+	private function mk_css(){
+		ob_start();?>
+		<style>
+			html, body {
+				background-color: #e7e7e7;
+				color: #333;
+				font-size: 16px;
+				margin: 0;
+				padding: 0;
+			}
+			.theme-container {
+				box-sizing: border-box;
+				text-align: center;
+				padding: 4em 20px;
+				margin: 30px auto;
+				width: calc(100% - 20px);
+				max-width: 600px;
+				background-color: #f6f6f6;
+				border: 1px solid #bbb;
+				border-radius: 5px;
+				box-shadow: 0 2px 12px rgba(0,0,0,0.1);
+			}
+			h1 {
+				font-size: 22px;
+			}
+			table{
+				margin: 0 auto;
+				max-width: 100%;
+			}
+			th {
+				text-align: right;
+				padding: 3px;
+			}
+			td {
+				text-align: left;
+				padding: 3px;
+			}
+			input[type=text],
+			input[type=password]{
+				display: inline-block;
+				box-sizing: border-box;
+				width: 160px;
+				min-width: 50px;
+				max-width: 100%;
+				padding: .375rem .75rem;
+				font-size: 1em;
+				font-weight: normal;
+				line-height: 1.5;
+				color: #333;
+				background-color: #f6f6f6;
+				background-clip: padding-box;
+				border: 1px solid #ced4da;
+				border-radius: .25rem;
+				transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+			}
+			input[type=text]:focus,
+			input[type=password]:focus{
+				color: #333;
+				background-color: #fff;
+				border-color: #80bdff;
+				outline: 0;
+				box-shadow: 0 0 0 .2rem rgba(0,123,255,.25);
+			}
+
+			button {
+				display: inline-block;
+				border-radius: 3px;
+				background-color: #f5fbfe;
+				color: #00a0e6;
+				border: 1px solid #00a0e6;
+				box-shadow: 0 2px 0px rgba(0,0,0,0.1);
+				padding: 0.5em 2em;
+				font-size:1em;
+				font-weight: normal;
+				line-height: 1;
+				text-decoration: none;
+				text-align: center;
+				cursor: pointer;
+				box-sizing: border-box;
+				align-items: stretch;
+				transition:
+					color 0.1s,
+					background-color 0.1s,
+					transform 0.1s
+				;
+			}
+			button:focus,
+			button:hover{
+				background-color: #d9f1fb;
+			}
+			button:hover{
+				background-color: #ccecfa;
+			}
+			button:active{
+				background-color: #00a0e6;
+				color: #fff;
+			}
+
+		</style>
+
+		<?php
+		$src = ob_get_clean();
+		return $src;
 	}
 
 }
